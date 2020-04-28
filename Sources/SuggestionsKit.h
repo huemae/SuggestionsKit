d9b94527711debe537beb0a410fe61d3cbaacee3
ilyailusha <ilyailusha@MacBook-Pro-ilyailusha.local>
2020-04-28 14:18:36 +0300

    update example and project update podspec

---
+++ Sources/SuggestionsKit.h
@@ -0,0 +1,19 @@
+//
+//  SuggestionsKit.h
+//  SuggestionsKit
+//
+//  Created by ilyailusha on 27.04.2020.
+//  Copyright © 2020 d. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SuggestionsKit.
+FOUNDATION_EXPORT double SuggestionsKitVersionNumber;
+
+//! Project version string for SuggestionsKit.
+FOUNDATION_EXPORT const unsigned char SuggestionsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuggestionsKit/PublicHeader.h>
+
+
